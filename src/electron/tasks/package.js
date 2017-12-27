b24d1edb6932ef42f27d6bc498d5cf9ebe5ccdbd
Umair Farooq <umair.farooq@blueeast.com>
2017-12-27 16:52:35 +0500

    feat(electron-linux-build): Script added for linux build of electron

--- src/electron/tasks/package.js
+++ src/electron/tasks/package.js
@@ -3,7 +3,7 @@
 const os = require('os');
 const packager = require('electron-packager');
 const argv = require('minimist')(process.argv.slice(2));
-const pkg = require('../../package.json');
+const pkg = require('../../../package.json');
 
 const shouldBuildAll = argv.all || false;
 
@@ -13,7 +13,7 @@ const options = {
 	appVersion: pkg.version,
 	asar: true,
 	dir: './electron/',
-	electronVersion: pkg.dependencies.electron.replace(/^\^/, ''),
+	electronVersion: pkg.devDependencies.electron.replace(/^\^/, ''),
 	icon: pkg.electron.icon,
 	ignore: [/^\/(?!build|package.json$)/],
 	name: pkg.electron.name || pkg.name,
