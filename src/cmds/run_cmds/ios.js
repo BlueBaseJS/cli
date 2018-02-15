adbf863de6c6a91439ae43ac5e94ac3b106c06e9
abdulbasitjs <mr_abdulbasit@live.com>
2018-02-15 19:00:52 +0500

    feat(Entry Point for Expo): User can pass entrypoint flag for bluerain run android/ios command like
    User can pass entrypoint flag for bluerain run android/ios command like this --entry=path

--- src/cmds/run_cmds/ios.js
+++ src/cmds/run_cmds/ios.js
@@ -6,5 +6,5 @@ exports.desc = 'Run a Bluerain project on iOS';
 exports.builder = {};
 exports.handler = function(argv) {
 	shell.echo(chalk.green('Running an iOS project! 🌏'));
-	require('../../expo/scripts/run');
+	require('../../expo/scripts/run')(argv);
 };
--- src/expo/scripts/run.js
+++ src/expo/scripts/run.js
@@ -1,13 +1,31 @@
-const { spawn } = require('child_process');
+const {
+	spawn
+} = require('child_process');
+const fs = require('fs');
+const path = require('path');
 const createAppJson = require('../createAppJson');
 const createAppJs = require('./createAppjs');
 const createBoot = require('./createBoot');
 const exp = require('./expo_cli')();
 
-createAppJson();
-createAppJs();
-createBoot();
+const isExistAppJS = fs.existsSync(path.resolve(process.cwd(), './App.js'));
 
-// spawn('exp', ['start', path.resolve( __dirname, '../../../')], { shell: true, stdio: 'inherit' });
-const execCommand = `${exp} start   `;
-spawn(execCommand, { shell: true, stdio: 'inherit' });
+module.exports = function(argv) {
+
+	if ((argv && argv.entry) && isExistAppJS) {
+		createAppJson(argv.entry);
+	} else if ((argv && argv.entry) && !isExistAppJS) {
+		throw new Error('Please create App.js');
+	} else {
+		createAppJson();
+		createAppJs();
+		createBoot();
+	}
+
+	// spawn('exp', ['start', path.resolve( __dirname, '../../../')], { shell: true, stdio: 'inherit' });
+	const execCommand = `${exp} start   `;
+	spawn(execCommand, {
+		shell: true,
+		stdio: 'inherit'
+	});
+};
