04bf1638c61d92c3710cf16c28f3ebac8a651673
Abdul Rehman Talat <rehman.talat@gmail.com>
2018-02-25 19:44:55 +0500

    refactor(app.js): Added seperate app.js in client

---
+++ packages/web/src/client/app.js
@@ -0,0 +1,5 @@
+import BR from '@blueeast/bluerain-os';
+import bootConfig from 'BLUERAIN_BOOT_CONFIG';
+
+const app = BR.boot(bootConfig);
+export default app;
--- packages/web/src/client/index.js
+++ packages/web/src/client/index.js
@@ -1,7 +1,1 @@
-import BR from '@blueeast/bluerain-os';
-// import bootConfig from 'CONFIG_PATH';
-import bootConfig from 'BLUERAIN_BOOT_CONFIG';
-
-BR.boot(bootConfig);
-// require('./web/registerServiceWorker');
-console.log('BR booted', BR);
+import './app';
