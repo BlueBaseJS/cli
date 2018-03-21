e501fcddbc282b336247ce3882de1edf938545a2
abdulbasitjs <mr_abdulbasit@live.com>
2018-03-21 17:13:13 +0500

    fix(Fix styling for bluerain boot page to make it center align): Fix styling for bluerain boot page
    Fix styling for bluerain boot page to make it center align

--- packages/web/src/shared/components/BlueRain/index.js
+++ packages/web/src/shared/components/BlueRain/index.js
@@ -1,3 +1,28 @@
+import React from 'react';
+import Helmet from 'react-helmet';
 import App from './app';
 
-export default App;
+const css = `
+    html, body, .app-container {
+        width: 100%;
+        height: 100%;
+        padding: 0;
+        border: none;
+        margin: 0;
+        font-family: proxima-nova, "Helvetica Neue", Helvetica, Roboto, Arial, sans-serif
+    }
+    *:focus {
+        outline: 0;
+    }
+`;
+
+const BluerainApp = () => (
+  <div style={{ width: '100%', height: '100%', cursor: 'default', display: 'flex' }}>
+    <Helmet>
+      <style>{css}</style>
+    </Helmet>
+    <App />
+  </div>
+);
+
+export default BluerainApp;
