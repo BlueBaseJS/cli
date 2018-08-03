7fddf888edcdafd8e59417830b5d16651b39fbce
Abdul Rehman Talat <rehman.talat@gmail.com>
2018-08-03 06:51:00 +0500

    fix(webpack): removed extra repos from vendor dll
    affects: @blueeast/bluerain-cli-web

--- packages/web/src/config/configFactory.js
+++ packages/web/src/config/configFactory.js
@@ -206,11 +206,11 @@ const configFactory = configs => customConfigs({
         // webpack loaders in order to be processed (e.g. CSS/SASS etc).
         // For these cases you don't want to include them in the Vendor DLL.
         include: [
-          'react-async-component',
+          // 'react-async-component',
           'react',
           'react-dom',
           'react-helmet',
-          'react-router-dom',
+          // 'react-router-dom',
         ],
 
         // The name of the vendor DLL.
