aa188db074ed70444f874b6e811327572324ba26
omarnaeem <omar.naeem@blueeast.com>
2018-10-03 09:56:54 +0500

    fix: checking if typings work now or not

---
+++ packages/core/src/typings.ts
@@ -0,0 +1,5 @@
+declare module '@blueeast/bluerain-cli-core/lib';
+declare module '@blueeast/bluerain-cli-core';
+declare module 'detect-installed';
+declare module 'file-regex';
+declare module 'yarn-install';
