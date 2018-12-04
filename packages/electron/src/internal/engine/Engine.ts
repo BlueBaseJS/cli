931ba444624ef0d99b7aa3d59e726b193a883c7d
omarnaeem <omar.naeem@blueeast.com>
2018-12-04 11:26:21 +0500

    fix: removed electron to passs travis for release

--- packages/electron/src/internal/engine/Engine.ts
+++
@@ -1,8 +0,0 @@
-import { ConfigFiles } from '../configFiles';
-import { Engine } from '@blueeast/bluerain-cli-core';
-
-export class ElectronEngine extends Engine {
-	name = 'Electron';
-	slug = 'electron';
-	configFiles = ConfigFiles;
-}
