d7d0116961e8a05f3f86557849fa49b3e209a7d9
Abdul Rehman Talat <rehman.talat@gmail.com>
2018-12-30 00:12:42 +0500

    feat(web): stable release

--- packages/web/templates/web/bluebase.ts
+++ packages/web/templates/web/bluebase.ts
@@ -7,16 +7,16 @@ import deepmerge from 'deepmerge';
  * We keep all the universal (cross platform) configs in 
  * the common folder, and extend them here.
  */
-const bootOptions: BootOptions = {
+const bootOptions: Partial<BootOptions> = {
 
-	config: {
+	// config: {
 
-		wallpaper: {
-			backgroundColor: 'white',
-			resizeMode: 'cover',
-			source: require('<%= ASSET_DIR_PATH %>/wallpaper.png'),
-		},
-	}
+	// 	wallpaper: {
+	// 		backgroundColor: 'white',
+	// 		resizeMode: 'cover',
+	// 		source: require('<%= ASSET_DIR_PATH %>/wallpaper.png'),
+	// 	},
+	// }
 };
 
 export default deepmerge(commonBootOptions, bootOptions);
