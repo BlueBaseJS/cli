61e55cdea0c1350ae0d68f0bba9752077abf606f
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-04-03 20:31:00 +0500

    feat(storybook-native): updated storybook native to latest version

---
+++ packages/storybook-native/typings/@bluebase/cli-expo.d.ts
@@ -0,0 +1,9 @@
+export const copyTemplateFiles: any;
+export const createBundle: any;
+export default _default;
+export const _default: {};
+export const expoVersions: any;
+export const getExpoSdk: any;
+export const getLatestExpoVersion: any;
+export const requiredDependencies: any;
+export const requiredDevDependencies: any;
