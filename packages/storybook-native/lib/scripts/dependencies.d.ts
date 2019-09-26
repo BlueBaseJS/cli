642cc6b181d573eab565051ab2e5f72a679adcc3
Abdul Rehman Talat <rehman.talat@gmail.com>
2019-09-26 13:01:30 +0500

    chore(storybook-native): Added lib to gitignore

--- packages/storybook-native/lib/scripts/dependencies.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * List of dependencies required by this plugin
- */
-export declare const requiredDependencies: any[];
-/**
- * List of dev dependencies required by this plugin
- */
-export declare const requiredDevDependencies: any[];
